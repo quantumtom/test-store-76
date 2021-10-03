e479fefc4880175c16b708089f206dadbba16aa2
Tom Cornyn <cornyn@gmail.com>
2021-10-03 12:51:22 -0700

    Modify data schema. Add collection item retrieval.

--- server.js
+++ server.js
@@ -18,9 +18,44 @@ app.disable('x-powered-by');
 // TODO:
 // Swtich to using Express router/routes
 
-app.patch('/v1/work/edit', (req, res) => {
+app.put('/v1/work/edit', (req, res) => {
   console.log('PATCH request to server');
   res.send('PATCH request to server');
+});
+
+app.post('/v1/work/:id', (req, res) => {
+  const requestBody = JSON.stringify(req.body);
+  res.status(200).send(`POST #2 works. 'req.params.id' is '${req.params.id}'.`);
+  fs.writeFileSync('work.json', requestBody);
+});
+
+app.get('/v1/get/advert/:id', (req, res) => {
+  const requestId = req.params.id;
+
+  fs.readFile('adverts.json', 'utf8',
+    (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      const adverts = JSON.parse(data);
+      const payload = adverts[requestId];
+
+      res.status(200).send(payload);
+    });
+});
+
+app.get('/v1/shorts/', (req, res) => {
+  fs.readFile('shorts.json', 'utf8',
+    (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      const payload = JSON.parse(data);
+
+      res.status(200).send(payload);
+    });
 });
 
 app.get('/v1/work/', (req, res) => {
@@ -48,29 +83,10 @@ app.post('/v1/work/create', (req, res) => {
   fs.writeFileSync('work.json', payload);
 });
 
-app.get('/v1/work/:id', (req, res) => {
+app.get('/v1/work/get/:id', (req, res) => {
   const requestId = req.params.id;
   res.status(200).send(`GET #2 works. 'req.params.id' is '${requestId}.'`);
   res.end();
-});
-
-app.post('/v1/work/:id', (req, res) => {
-  const requestBody = JSON.stringify(req.body);
-  res.status(200).send(`POST #2 works. 'req.body' is '${requestBody}'.`);
-  fs.writeFileSync('work.json', requestBody);
-});
-
-app.get('/v1/shorts/', (req, res) => {
-  fs.readFile('shorts.json', 'utf8',
-    (err, data) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      const payload = JSON.parse(data);
-
-      res.status(200).send(payload);
-    });
 });
 
 app.post('/v1/shorts/create', (req, res) => {
