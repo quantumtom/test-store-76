360f9c84de5ea1f3a5e60a4609befdb22c05a42c
Tom Cornyn <cornyn@gmail.com>
2021-10-24 14:22:09 -0700

    Allow Heroku to set the port number for the Express service.

--- Server.js
+++ Server.js
@@ -2,6 +2,7 @@ const Router = require('./Router');
 const SERVER_PORT = process.env.PORT || '8080';
 
 exports.start = () => {
+  Router.set('port', SERVER_PORT)
   Router.listen(SERVER_PORT,
     () => console.log(`the router is alive on port:${SERVER_PORT}`)
   )
