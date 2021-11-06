a9d9d4081d7fbf048eaae38c4f40e08c5f83588a
Tom Cornyn <cornyn@gmail.com>
2021-11-05 20:21:20 -0700

    Update tests.

--- routes/vTwo.adverts.routes.test.js
+++ routes/vTwo.adverts.routes.test.js
@@ -3,11 +3,13 @@ const supertest = require('supertest');
 const fs = require("fs");
 const path = require("path")
 const filePath = path.resolve(__dirname, "../vTwo.Adverts.json");
+const testFilePath = path.resolve(__dirname, "../adverts.routes.test.data.json");
 
-let simulata = {};
-
-(() => {
-  fs.readFileSync(filePath, {encoding: "utf8"},
+fs.copyFile(filePath, testFilePath, (err) => {
+  if (err) {
+    console.error(err);
+  }
+  fs.readFile(testFilePath, {encoding: "utf8"},
     (err, store) => {
       if (err) {
         console.error(err);
@@ -16,14 +18,41 @@ let simulata = {};
 
       simulata = JSON.parse(store);
     });
-})();
+});
+
+let simulata = {};
 
 describe('v2 adverts read list endpoint', () => {
+  const PATH = '/v2/adverts'
   it('should return status 302', (done) => {
     supertest(Router)
-      .get('/v2/adverts/')
+      .get(PATH)
       .set('Accept', 'application/json')
       .expect(302)
+      .end((err) => {
+        if (err) {
+          return done(err);
+        }
+        return done();
+      })
+  })
+})
+
+describe('v2 adverts put (one) endpoint', () => {
+  const PUT_PATH = '/v2/adverts/1'
+  const dataEdit = {
+    "jobID": "9",
+    "videoID": "117235079",
+    "title": "MERCEDES-BENZ",
+    "description": "Record  (4th Unit Photography)"
+  };
+
+  it('should return status 200', (done) => {
+    supertest(Router)
+      .put(PUT_PATH)
+      .set('Accept', 'application/json')
+      .send(dataEdit)
+      .expect(200)
       .end((err) => {
         if (err) {
           return done(err);
@@ -36,7 +65,7 @@ describe('v2 adverts read list endpoint', () => {
 describe('v2 adverts read one endpoint', () => {
   it('should return status 302', (done) => {
     supertest(Router)
-      .get('/v2/adverts/1/')
+      .get('/v2/adverts/1')
       .set('Accept', 'application/json')
       .expect(302)
       .end((err) => {
@@ -49,7 +78,7 @@ describe('v2 adverts read one endpoint', () => {
 
   it('should return content-type JSON', (done) => {
     supertest(Router)
-      .get('/v2/adverts/1/')
+      .get('/v2/adverts/1')
       .set('Accept', 'application/json')
       .end((err) => {
         if (err) {
@@ -61,7 +90,7 @@ describe('v2 adverts read one endpoint', () => {
 
   it("should return an exact copy of the data payload that was sent.", (done) => {
     supertest(Router)
-      .get('/v2/adverts/1/')
+      .get('/v2/adverts/1')
       .set('Accept', 'application/json')
       .expect((res) => {
         expect(res.body.data).toStrictEqual(simulata.data);
@@ -73,4 +102,8 @@ describe('v2 adverts read one endpoint', () => {
         return done();
       })
   });
+});
+
+fs.unlink(testFilePath, (err) => {
+  if (err) throw err;
 });
