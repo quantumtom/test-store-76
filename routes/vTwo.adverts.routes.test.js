fd2815add6f641858a9a2c5ce14a08c94bc78401
quantumtom <cornyn@gmail.com>
2021-12-14 11:00:06 -0800

    Adjust implementation of PUT and DELETE tests.

--- routes/vTwo.adverts.routes.test.js
+++ routes/vTwo.adverts.routes.test.js
@@ -2,17 +2,16 @@ const Router = require('./../Router');
 const supertest = require('supertest');
 const fs = require("fs");
 const path = require("path")
-const filePath = path.resolve(__dirname, "../vTwo.shorts.json");
-const testFilePath = path.resolve(__dirname, "../shorts.routes.test.data.json");
+const filePath = path.resolve(__dirname, "../vTwo.adverts.json");
+const testFilePath = path.resolve(__dirname, "../adverts.routes.test.data.json");
 
 let simulata = {};
 
-const dataEdit = {
+let testItem = {
   "videoID": "999999999",
   "title": "***TEST_TITLE***",
-  "description": "***TEST_DESCRIPTION***",
-  "position": 999
-};
+  "description": "***TEST_DESCRIPTION***"
+}
 
 
 fs.copyFile(filePath, testFilePath, (err) => {
@@ -35,9 +34,7 @@ describe('v2 adverts GET endpoint', () => {
     supertest(Router)
       .get('/v2/adverts')
       .set('Accept', 'application/json')
-      .expect((res) => {
-        expect(res.status).toEqual(200);
-      })
+      .expect(200)
       .end((err) => {
         if (err) {
           return done(err);
@@ -48,11 +45,9 @@ describe('v2 adverts GET endpoint', () => {
 
   it('should return status 200', (done) => {
     supertest(Router)
-      .get('/v2/adverts/1')
+      .get('/v2/adverts/clips/1')
       .set('Accept', 'application/json')
-      .expect((res) => {
-        expect(res.status).toEqual(200);
-      })
+      .expect(200)
       .end((err) => {
         if (err) {
           return done(err);
@@ -63,7 +58,7 @@ describe('v2 adverts GET endpoint', () => {
 
   it("should return an exact copy of the data payload that was sent.", (done) => {
     supertest(Router)
-      .get('/v2/adverts/1')
+      .get('/v2/adverts')
       .set('Accept', 'application/json')
       .expect((res) => {
         expect(res.body.data).toStrictEqual(simulata.data);
@@ -80,43 +75,9 @@ describe('v2 adverts GET endpoint', () => {
 describe('v2 adverts POST endpoint', () => {
   it('should return status 201', (done) => {
     supertest(Router)
-      .post('/v2/adverts')
-      .send(dataEdit)
+      .post('/v2/adverts/clips')
+      .send(testItem)
       .set('Accept', 'application/json')
-      .expect((res) => {
-        expect(res.status).toEqual(201);
-      })
-      .end((err) => {
-        if (err) {
-          return done(err);
-        }
-        return done();
-      })
-  })
-})
-
-describe('v2 adverts PUT endpoint', () => {
-
-  it('should return status 201', (done) => {
-    supertest(Router)
-      .put('/v2/adverts/99')
-      .set('Content-Type', 'application/json')
-      .send(dataEdit)
-      .expect(201)
-      .expect('Content-Type', /json/)
-      .end((err) => {
-        if (err) {
-          return done(err);
-        }
-        return done();
-      })
-  })
-
-  it('should return status 201', (done) => {
-    supertest(Router)
-      .put('/v2/adverts/9999')
-      .set('Content-Type', 'application/json')
-      .send(dataEdit)
       .expect(201)
       .end((err) => {
         if (err) {
@@ -127,10 +88,24 @@ describe('v2 adverts PUT endpoint', () => {
   })
 })
 
-describe('v2  adverts DELETE endpoint', () =>  {
+describe('v2 adverts clips PUT/DELETE endpoints', () =>  {
+  it('should return status 201', (done) => {
+    supertest(Router)
+      .put('/v2/adverts/clips/999999999')
+      .set('Content-Type', 'application/json')
+      .send(testItem)
+      .expect(201)
+      .end((err) => {
+        if (err) {
+          return done(err);
+        }
+        return done();
+      })
+  })
+
   it('should return status 202', (done) => {
     supertest(Router)
-      .delete('/v2/adverts/999999999')
+      .delete('/v2/adverts/clips/999999999')
       .set('Accept', 'application/json')
       .expect(202)
       .end((err) => {
@@ -141,11 +116,11 @@ describe('v2  adverts DELETE endpoint', () =>  {
       })
   })
 
-  it('should return status 2XX', (done) => {
+  it('should return status 404', (done) => {
     supertest(Router)
-      .delete('/v2/adverts/999')
+      .delete('/v2/adverts/clips/000000000')
       .set('Accept', 'application/json')
-      .expect(200)
+      .expect(404)
       .end((err) => {
         if (err) {
           return done(err);
@@ -155,6 +130,6 @@ describe('v2  adverts DELETE endpoint', () =>  {
   })
 });
 
-fs.unlink(testFilePath, (err) => {
-  if (err) throw err;
-});
+// fs.unlink(testFilePath, (err) => {
+//   if (err) throw err;
+// });
