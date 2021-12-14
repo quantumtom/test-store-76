45d22a36328f064b9729c9fcdb591598d4f79973
quantumtom <cornyn@gmail.com>
2021-12-14 14:42:39 -0800

    Add route for PUT requests without resource identifiers.

--- routes/vTwo.adverts.routes.js
+++ routes/vTwo.adverts.routes.js
@@ -6,6 +6,7 @@ advertsRouter.get('/', advertsController.readListRequest)
 advertsRouter.post('/', advertsController.createListRequest)
 
 advertsRouter.post('/clips', advertsController.addItemRequest)
+advertsRouter.put('/clips', advertsController.addItemRequest)
 advertsRouter.put('/clips/:guid', advertsController.replaceItemRequest)
 advertsRouter.get('/clips/:guid', advertsController.readItemRequest)
 advertsRouter.delete('/clips/:guid', advertsController.deleteItemRequest)
