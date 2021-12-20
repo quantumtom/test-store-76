38deef83556368aa50cc8ec1d13957a63ab5eac3
quantumtom <cornyn@gmail.com>
2021-12-20 10:40:18 -0800

    Remove references to API version one.

--- Router.js
+++ Router.js
@@ -2,8 +2,6 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
-const vOneAdvertsRoutes = require('./routes/vOne.adverts.routes')
-const vOneShortsRoutes = require('./routes/vOne.shorts.routes')
 const advertsRoutes = require('./routes/vTwo.adverts.routes')
 const shortsRoutes = require('./routes/vTwo.shorts.routes')
 
@@ -15,8 +13,6 @@ app.use(express.json( {
   strict: true
 } ))
 
-app.use('/v1/adverts/', vOneAdvertsRoutes)
-app.use('/v1/shorts/', vOneShortsRoutes)
 app.use('/v2/adverts/', advertsRoutes)
 app.use('/v2/shorts/', shortsRoutes)
 
